c864ca806c12380beb5018b174aba041056d4d7b
prachithakkar218 <prachithakkar218@gmail.com>
2019-02-22 16:43:41 -0600

    test cases 2,3,4 and added sort by locator

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,6 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
 public class HomePage extends BasePage{
 
     //Locators
+    By sortBySelect = By.cssSelector("#selectProductSort");
 
     //Action Methods
+    public void selectSortByOptions(int option)  {
+        WebElement element = driver.findElement(sortBySelect);
+        Select select = new Select(element);
+        select.selectByIndex(option);
+    }
+
 }
--- src/main/java/SearchPage.java
+++ src/main/java/SearchPage.java
@@ -4,9 +4,14 @@ import org.openqa.selenium.WebElement;
 public class SearchPage extends BasePage {
 
     By resultText = By.cssSelector("span.lighter");
+    By resultsMessage = By.className("heading-counter");
 
     public String getResultHeaderText() {
         WebElement headerText = driver.findElement(resultText);
         return headerText.getText();
+    }
+    public String getResultsMessage()  {
+        WebElement resultMessageElement = driver.findElement(resultsMessage);
+        return resultMessageElement.getText();
     }
 }
